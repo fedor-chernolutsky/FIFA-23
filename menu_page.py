0e51867600810c19d55224a6e9de652d205e882c
fedor2pac <frc123010702@yandex.ru>
2023-01-22 20:05:09 +0300

    Mini logo

--- menu_page.py
+++ menu_page.py
@@ -7,6 +7,13 @@ class MenuPage:
     def __init__(self, clock, screen):
         self.clock = clock
         self.screen = screen
+        self.grass_color = (173, 206, 129)
+        
+        self.logo = pygame.transform.scale(pygame.image.load("FIFA23.png"), (256, 60))
+        self.white = pygame.Surface((256, 60))
+        self.white.fill(WHITE)
+        self.logo.blit(self.white, (0, 0), special_flags=pygame.BLENDMODE_BLEND)
+        
     
     def run(self, pages):
 
@@ -17,5 +24,6 @@ class MenuPage:
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     quit()
             
-            self.screen.fill(WHITE)
+            self.screen.fill(self.grass_color)
+            self.screen.blit(self.logo, (10, 10))
             pygame.display.update()
