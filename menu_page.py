f01d52045e71fa96481687ba413c3b786e2e78d7
fedor2pac <frc123010702@yandex.ru>
2023-03-09 13:33:52 +0300

    так кое-что

--- menu_page.py
+++ menu_page.py
@@ -7,13 +7,11 @@ class MenuPage:
     def __init__(self, screen):
         self.clock = pygame.time.Clock()
         self.screen = screen
-        self.grass_color = (173, 206, 129)
+        self.GRASS_COLOR = (173, 206, 129)
         
-        self.logo = pygame.transform.scale(pygame.image.load("FIFA23.png"), (256, 60))
-        self.white = pygame.Surface((256, 60))
-        self.white.fill(WHITE)
-        self.logo.blit(self.white, (0, 0), special_flags=pygame.BLENDMODE_BLEND)
-        
+        self.logo = pygame.transform.scale(pygame.image.load("FIFA23.png"), (256, 60))    
+        white = pygame.Surface((256, 60)); white.fill(WHITE)
+        self.logo.blit(white, (0, 0), special_flags=pygame.BLENDMODE_BLEND)
     
     def run(self, pages):
 
@@ -24,6 +22,6 @@ class MenuPage:
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     quit()
             
-            self.screen.fill(self.grass_color)
+            self.screen.fill(self.GRASS_COLOR)
             self.screen.blit(self.logo, (10, 10))
             pygame.display.update()
